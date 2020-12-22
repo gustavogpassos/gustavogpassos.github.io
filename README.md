7d719b4fe9e829c7ed52cdc91f81427487ee3b9c
Gustavo <girardigustavo.03@gmail.com>
2020-12-21 22:26:51 -0300

    Functional application, visual improvement

---
+++ README.md
@@ -0,0 +1,5 @@
+## Count down timer ##
+
+** projeto criado para estudo desenvolvendo um timer para contagem regressiva de datas **
+
+** também será implementado um contador de tempo em horas, minutos e segundos **
--- script.js
+++ script.js
@@ -1,12 +1,63 @@
-function calctimeto(time) {
-    console.log(time);
+loadEventListeners();
 
-    today = new Date();
+function loadEventListeners() {
+	document.addEventListener('DOMContentLoaded', function() { calctimeto(); });
+};
 
-    console.log(today.getTime());
+
+var timeTo = document.getElementById('target-date').value,
+        date,
+		now = new Date(),
+		newYear = new Date('1.1.2020').getTime(),
+		startTimer = '';
+
+function calctimeto(finalDate) {
+    //console.log(finalDate);
+    
+    //var finalDate = new Date(date).getTime();
+
+    clearInterval(startTimer);
+
+    if(typeof(finalDate) == 'undefined'){
+        date = new Date(newYear).getTime();
+    }else {
+        date = new Date(finalDate).getTime();
+    }
+
+    function updateTimer(finalDate) {        
+
+        var now = new Date().getTime();
+
+        var difference = date - now;
+
+        var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((difference % (1000 * 60 * 60) / (1000 * 60)));
+        var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+        //console.log(days);
+
+        document.querySelector('.clock-days').innerHTML = days;
+        document.querySelector('.clock-hours').innerHTML = hours;
+        document.querySelector('.clock-minutes').innerHTML = minutes;
+        document.querySelector('.clock-seconds').innerHTML = seconds;
+
+
+        if(now >= date){
+			clearInterval(startTimer);
+			document.querySelector('.clock-days').innerHTML = '0';
+			document.querySelector('.clock-hours').innerHTML = '0';
+			document.querySelector('.clock-minutes').innerHTML = '0';
+			document.querySelector('.clock-seconds').innerHTML = '0';
+		}
+    }
+
+    startTimer = setInterval(function(){updateTimer(date);}, 1000);
+    
 }
 
 
+//common timer for minutes countdown
 function timer() {
     minutes = document.getElementById("minutes").value;
     secconds = document.getElementById("secconds").value;
