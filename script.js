2456cde203aa7cc389decf8fa0e089363cbf861a
Gustavo <girardigustavo.03@gmail.com>
2021-01-13 23:02:18 -0300

    Criado gerador de senhas, navegação entre abas*

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 loadEventListeners();
 
 function loadEventListeners() {
-	//document.addEventListener('DOMContentLoaded', function() { calctimeto(); });
+    //document.addEventListener('DOMContentLoaded', function() { calctimeto(); });
 };
 
 
@@ -10,41 +10,40 @@ var d = new Date().getUTCDate();
 var m = new Date().getUTCMonth();
 var y = new Date().getFullYear();
 
-d +=1;
-m +=1;
+d += 1;
+m += 1;
 
-hoje = y+'-'+m+'-'+d;
+hoje = y + '-' + m + '-' + d;
 
 //console.log(hoje);
 
-document.getElementById('target-date').setAttribute('min',hoje);
+document.getElementById('target-date').setAttribute('min', hoje);
 
 
 var timeTo = document.getElementById('target-date').value,
-        date,
-		now = new Date(),
-		newYear = new Date('1.1.2020').getTime(),
-		startTimer = '';
+    date,
+    now = new Date(),
+    newYear = new Date('1.1.2020').getTime(),
+    startTimer = '';
 
 function calctimeto(targetDate) {
     //console.log(finalDate);
-    
+
     //var finalDate = new Date(date).getTime();
 
     clearInterval(startTimer);
 
-    if(typeof(targetDate) == 'undefined'){
+    if (typeof (targetDate) == 'undefined') {
         date = new Date(newYear).getTime();
-    }else {
+    } else {
         date = new Date(targetDate);
-        date.setTime( date.getTime() + date.getTimezoneOffset() * 60 * 1000 + (0) * 60 * 60 * 1000);
+        date.setTime(date.getTime() + date.getTimezoneOffset() * 60 * 1000 + (0) * 60 * 60 * 1000);
         console.log(date);
         var dataCerta = date.getTime();
-        
     }
 
-    function updateTimer(finalDate) {    
-        
+    function updateTimer(finalDate) {
+
         //finalDate.setTime( finalDate.getTime() + finalDate.getTimezoneOffset() * 60 * 1000 + (-3) * 60 * 60 * 1000)
 
         var now = new Date();
@@ -70,17 +69,17 @@ function calctimeto(targetDate) {
         document.querySelector('.clock-seconds').innerHTML = seconds;
 
 
-        if(newNow >= targetDate){
-			clearInterval(startTimer);
-			document.querySelector('.clock-days').innerHTML = '0';
-			document.querySelector('.clock-hours').innerHTML = '0';
-			document.querySelector('.clock-minutes').innerHTML = '0';
-			document.querySelector('.clock-seconds').innerHTML = '0';
-		}
+        if (newNow >= targetDate) {
+            clearInterval(startTimer);
+            document.querySelector('.clock-days').innerHTML = '0';
+            document.querySelector('.clock-hours').innerHTML = '0';
+            document.querySelector('.clock-minutes').innerHTML = '0';
+            document.querySelector('.clock-seconds').innerHTML = '0';
+        }
     }
 
-    startTimer = setInterval(function(){updateTimer(dataCerta);}, 1000);
-    
+    startTimer = setInterval(function () { updateTimer(dataCerta); }, 1000);
+
 }
 
 
@@ -96,4 +95,48 @@ function timer() {
     } else {
         window.alert('your timer is ' + minutes + ' minutes and ' + secconds + ' secconds');
     }
+}
+
+
+function generatePassword() {
+    var isSelected = 0;
+
+    var mainString = '';
+
+    var upperOption = document.getElementById('upper').checked;
+    var lowerOption = document.getElementById('lower').checked;
+    var numberOption = document.getElementById('number').checked;
+    var specialOption = document.getElementById('special').checked;
+    var passLenght = document.getElementById('lenght').value;
+
+    if (upperOption) {
+        mainString += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        isSelected += 1;
+    }
+    if (lowerOption) {
+        mainString += 'abcdefghijklmnopqrstuvwxyz';
+        isSelected += 1;
+    }
+    if (numberOption) {
+        mainString += '0123456789';
+        isSelected += 1;
+    }
+    if (specialOption) {
+        mainString += '!@#$*.-+';
+        isSelected += 1;
+    }
+
+    if (isSelected == 0) {
+        window.alert('Selecione pelo menos uma opção.')
+    } else if (passLenght < 8) {
+        window.alert('O tamanho mínimo para a senha é 8 digitos');
+    } else {
+        var shuffled = mainString.split('').sort(function () { return 0.5 - Math.random() }).join('');
+        var genPassword = '';
+        for (var i = 0; i <= passLenght; i++) {
+            genPassword += shuffled.charAt(Math.random() * passLenght);
+        }
+        document.getElementById('generated-password').value = genPassword;
+    }
+
 }
