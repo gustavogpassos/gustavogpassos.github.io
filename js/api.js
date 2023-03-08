4f36096ef6efab324568c40456d2716c387fae08
Gustavo G Passos <girardi_gustavo@outlook.com>
2023-03-07 23:31:26 -0300

    busca de repositórios, detalhes do repositório

--- js/api.js
+++ js/api.js
@@ -14,11 +14,30 @@ const api = {
     searchUserRepos: async (user, search) => {
         const queryString =
             "q=" + encodeURIComponent(`${search} in:name user:${user}`)
-        const response = await app.request(`GET /repos?${queryString}`)
+        const response = await app.request(
+            `GET /search/repositories?${queryString}`
+        )
         return response.data
     },
     getRepoInfo: async (user, repo) => {
-        const response = await app.request(`GET /repos/${user}/${repo}`)
+        const commits = await app.request(`GET /repos/${user}/${repo}/commits`)
+        const lastCommitMsg = commits.data[0].commit.message
+        const lastCommitAuthor = commits.data[0].commit.author.name
+        const lastCommitDate = new Date(
+            commits.data[0].commit.author.date
+        ).toLocaleString("pt-BR")
+        const languages = await app.request(
+            `GET /repos/${user}/${repo}/languages`
+        )
+
+        return {
+            name: repo,
+            countCommits: commits.data.length,
+            lastCommitMsg,
+            lastCommitAuthor,
+            lastCommitDate,
+            languages: Object.keys(languages.data).join(", "),
+        }
     },
 }
 
--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ import api from "./api.js"
 
 $(async () => {
     $("#loading").show()
-    const search = $("#search").val()
     const user = await api.searchUser("gustavogpassos")
     if (user != null) {
         const repos = await api.getUserRepos(user.login)
@@ -10,28 +9,59 @@ $(async () => {
         $("#user-name").html(user.login)
         $("#user-image").attr("src", user.avatar_url)
         $("#count-repos").html(repos.length)
+        sessionStorage.setItem("user", user.login)
     }
     $("#loading").hide()
 })
 
 $("#repoDetail").on("click", async (data) => {
+    $("#list-repo-header").hide()
     console.log(data)
 })
 
+$("#search-repo-btn").on("click", async () => {
+    $("#loading").show()
+    let search = $("#search-repo").val()
+    if (search.trim() != "") {
+        $("#search-repo").removeClass("is-invalid")
+        const user = sessionStorage.getItem("user")
+        const repos = await api.searchUserRepos(user, search)
+        console.log(repos)
+        renderRepos(repos.items)
+    } else {
+        $("#search-repo").addClass("is-invalid")
+    }
+    $("#loading").hide()
+})
+
+$("#clear-filter-btn").on("click", async () => {
+    $("#loading").show()
+    $("#search-repo").val("")
+    const user = await api.searchUser("gustavogpassos")
+    if (user != null) {
+        const repos = await api.getUserRepos(user.login)
+        renderRepos(repos)
+        $("#user-name").html(user.login)
+        $("#user-image").attr("src", user.avatar_url)
+        $("#count-repos").html(repos.length)
+        sessionStorage.setItem("user", user.login)
+    }
+    $("#loading").hide()
+})
+
 function renderRepos(repos) {
-    console.log(repos)
+    $("#list-repos").html("")
     repos.map((repo) => {
         //criar link
         var cardLink = document.createElement("a")
-        cardLink.href = "#"
+        cardLink.href = "#!"
         cardLink.addEventListener("click", getRepoInfo)
-        cardLink.id = repo.id
+        cardLink.id = repo.name
         cardLink.innerHTML = "Ver mais"
         cardLink.className = "card-link position-absolute"
         //titulo
         var cardTitle = document.createElement("h5")
         cardTitle.className = "card-title text-truncate"
-        cardTitle.setAttribute("alt", repo.name)
         cardTitle.innerHTML = repo.name
         //subtitulo
         var cardSubtitle = document.createElement("h6")
@@ -56,6 +86,47 @@ function renderRepos(repos) {
     })
 }
 
-function getRepoInfo(element) {
-    console.log(element.target.id)
+async function getRepoInfo(element) {
+    $("#loading").show()
+    const user = sessionStorage.getItem("user")
+    const repo = await api.getRepoInfo(user, element.target.id)
+
+    console.log(repo)
+    //limpar os campos
+    $("#repo-name").html("")
+    $("#repo-langs").html("")
+    $("#commit-count").html("")
+    $("#last-commit").html("")
+
+    $("#repo-name").html(repo.name)
+    $("#repo-langs").html("Linguagens: " + repo.languages)
+    $("#commit-count").html("Commits: " + repo.countCommits)
+
+    const list = document.createElement("ul")
+    const cAuthor = document.createElement("li")
+    cAuthor.innerHTML = "Autor: " + repo.lastCommitAuthor
+    const cDate = document.createElement("li")
+    cDate.innerHTML = "Data: " + repo.lastCommitDate
+    const cMessage = document.createElement("li")
+    cMessage.className = "text-truncate"
+    cMessage.innerHTML = "Mensagem: " + repo.lastCommitMsg
+
+    list.appendChild(cAuthor)
+    list.appendChild(cDate)
+    list.appendChild(cMessage)
+
+    const strong = document.createElement("strong")
+    strong.innerHTML = "Último commit"
+    $("#last-commit").append(strong)
+    $("#last-commit").append(list)
+
+    $("#list-repos-main").hide()
+    $("#detail-repo-main").show()
+
+    $("#loading").hide()
 }
+
+$("#back-list").on("click", () => {
+    $("#list-repos-main").show()
+    $("#detail-repo-main").hide()
+})
